a5ca060eeb19eb2459bed0725dbe8fff784137c9
Dawid Lebiedzki <lebiedzki@gmail.com>
2020-03-31 21:37:44 +0200

    feat: add form updae roles

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,7 +8,17 @@
             <div class="card-header">Edit user {{ $user->name }}</div>
 
                 <div class="card-body">
-                  
+                <form action="{{ route('admin.users.update', $user) }}" method="POST">
+                    @csrf
+                    {{ method_field('PUT') }}
+                    @foreach ($roles as $role)
+                        <div class="form-check">
+                        <input type="checkbox" name="roles[]" value="{{$role->id}}">
+                        <label>{{ $role->name }}</label>
+                        </div>
+                    @endforeach
+                        <button type="submit" class="btn btn-primary">Update</button>
+                </form>
                  
                 </div>
             </div>
