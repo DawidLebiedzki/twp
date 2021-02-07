22a387a07534247ee58d9892bce568460cc970ee
Dawid Lebiedzki <lebiedzki@gmail.com>
2021-02-07 09:00:35 +0100

    deleted middleware restriction

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     /**
      * Show the application dashboard.
@@ -23,6 +17,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $permission = Permission::create(['name' => 'edit user']);
+        $role = Role::findById(2);
+
+        // $permission = Permission::findById(1);
+        $role->givePermissionTo($permission);
         return view('home');
     }
 }
