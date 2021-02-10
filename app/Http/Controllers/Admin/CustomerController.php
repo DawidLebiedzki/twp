2c2129ce77e7cc75eddebbab27b106540639aa46
Dawid Lebiedzki <lebiedzki@gmail.com>
2021-02-10 09:15:47 +0100

    added index function

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Customer;
 
 class CustomerController extends Controller
 {
@@ -14,7 +15,8 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        $customers = Customer::all();
+        return view('admin.customers.index', compact('customers'));
     }
 
     /**
