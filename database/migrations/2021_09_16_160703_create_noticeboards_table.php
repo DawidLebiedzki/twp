83fef2eea9a792ab74d474e404ec1de3c761af86
Dawid Lebiedzki <lebiedzki@gmail.com>
2021-09-16 18:17:22 +0200

    New migration

--- database/migrations/2021_09_16_160703_create_noticeboards_table.php
+++ database/migrations/2021_09_16_160703_create_noticeboards_table.php
@@ -15,7 +15,13 @@ class CreateNoticeboardsTable extends Migration
     {
         Schema::create('noticeboards', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
+        });
+
+        Schema::table('noticeboards', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
